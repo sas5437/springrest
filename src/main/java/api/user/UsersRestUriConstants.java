84c588030b49715290b0ec140f84f958bc4096d9
Scott Serok <sserok@grubhub.com>
2017-06-19 17:27:12 -0400

    align url strings

--- src/main/java/api/user/UsersRestUriConstants.java
+++ src/main/java/api/user/UsersRestUriConstants.java
@@ -1,9 +1,9 @@
 package api.user;
 
 public class UsersRestUriConstants {
-  public static final String INDEX = "/api/v1/users";
+  public static final String INDEX  = "/api/v1/users";
   public static final String CREATE = "/api/v1/users";
-  public static final String READ = "/api/v1/users/{id}";
+  public static final String READ   = "/api/v1/users/{id}";
   public static final String UPDATE = "/api/v1/users/{id}";
   public static final String DELETE = "/api/v1/users/{id}";
 }
